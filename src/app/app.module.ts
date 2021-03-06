62dfd62c4bb74bb5e7d3c028f14dc8f8c722671f
Michael Bradvica <mjbradvica@protonmail.com>
2021-03-06 05:06:51 -0800

    routing and home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from 'src/features/home/home.component';
+
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule],
+	declarations: [AppComponent, HomeComponent],
+	imports: [BrowserModule, RouterModule.forRoot(routes)],
 	providers: [],
 	bootstrap: [AppComponent],
 })
